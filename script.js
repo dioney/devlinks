e14b99b5f782be775077945a0838f6be4ef41df8
Dioney Vitor <dioneyvitor@gmail.com>
2023-06-27 09:21:52 -0300

    Add a README

--- script.js
+++ script.js
@@ -6,7 +6,15 @@ function toggleMode() {
 
   if (html.classList.contains("light")) {
     img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute(
+      "alt",
+      "Foto do Dioney de óculos escuro camisa clara e no fundo o horizonte do mar."
+    )
   } else {
     img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute(
+      "alt",
+      "Foto de Dioney de braços cruzados com camisa cinza escuro e um fundo azul rosa."
+    )
   }
 }
